a2e671382adbee8567b1991b95d13c879e1128ae
William Damascena <williamenezes@outlook.com>
2024-07-29 12:51:26 -0300

    upload files

---
+++ src/main/java/app/vercel/ceaiapp/controller/FirstController.java
@@ -0,0 +1,15 @@
+package app.vercel.ceaiapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "hello-world")
+public class FirstController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello world";
+    }
+}
