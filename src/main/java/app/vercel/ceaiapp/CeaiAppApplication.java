e4dbadf0bb9aa998797a0364257dc57189adedb2
William Menezes Damascena <williamenezes@outlook.com>
2024-10-12 08:06:19 -0300

    feat: Adicionando configurações do banco de dados

---
+++ src/main/java/app/vercel/ceaiapp/CeaiAppApplication.java
@@ -0,0 +1,13 @@
+package app.vercel.ceaiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeaiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeaiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/vercel/ceaiapp/CeaiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.vercel.ceaiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeaiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
