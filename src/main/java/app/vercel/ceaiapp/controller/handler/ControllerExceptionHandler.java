08c6c65a19dd9b30d35ed5ae07a5ceebb740e0c6
William Menezes Damascena <williamenezes@outlook.com>
2025-02-17 15:10:14 -0300

    :sparkles: feat: Adicionando ValidationError

--- src/main/java/app/vercel/ceaiapp/controller/handler/ControllerExceptionHandler.java
+++ src/main/java/app/vercel/ceaiapp/controller/handler/ControllerExceptionHandler.java
@@ -1,12 +1,15 @@
 package app.vercel.ceaiapp.controller.handler;
 
 import app.vercel.ceaiapp.dto.CustomError;
+import app.vercel.ceaiapp.dto.ValidationError;
 import app.vercel.ceaiapp.service.exception.DatabaseException;
 import app.vercel.ceaiapp.service.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,6 +29,18 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(),  "Dados inválidos", request.getRequestURI());
+
+        for(FieldError f: e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
         return ResponseEntity.status(status).body(err);
     }
 }
--- src/main/java/app/vercel/ceaiapp/entity/Endereco.java
+++ src/main/java/app/vercel/ceaiapp/entity/Endereco.java
@@ -17,17 +17,17 @@ public class Endereco {
     @Column(name = "endereco_id")
     private Long id;
 
-    @NotBlank
-    @Size(min = 5, max = 255)
+    @NotBlank(message = "Campo obrigatório")
+    @Size(min = 5, max = 255, message = "Campo deve ter no mínimo 5 caracteres")
     private String logradouro;
 
-    @Positive
+    @Positive(message = "O número deve ser positivo")
     private int numero;
 
-    @Size(max = 100)
+    @Size(max = 100, message = "Complemento deve ter no máximo 100 caracteres ")
     private String complemento;
 
-    @Size(max = 10)
+    @Size(max = 10, message = "CEP deve ter 10 caracteres")
     private String cep;
 
     @Size(max = 100)
